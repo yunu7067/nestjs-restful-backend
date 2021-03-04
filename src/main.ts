57e50b371af3d1b496646d5368e235e9b4533841
yunu7067 <yunu7067@gmail.com>
2021-03-04 20:44:33 +0900

    hotfix

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  let port = process.env.PORT ?? 3000;
+  const port = process.env.PORT ?? 3000;
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
 
