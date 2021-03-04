2fcba1f832218fb176fe7b4826230923e7f96cf3
yunu7067 <yunu7067@gmail.com>
2021-03-04 21:58:30 +0900

    setting client static folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,7 @@ import { join } from 'path';
 @Module({
   imports: [
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '../client', 'public'),
+      rootPath: join(__dirname, '..', 'client', 'build'),
     }),
     GraphQLModule.forRoot({
       installSubscriptionHandlers: true,
