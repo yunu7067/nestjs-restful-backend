2518fd0229a98fc72a349263803da249768b3d5a
최윤혁 <57068092+yunu7067@users.noreply.github.com>
2021-03-04 23:14:01 +0900

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,10 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 
 @Module({
-  imports: [
+  imports: [    
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client', 'build'),
+    }),
     GraphQLModule.forRoot({
       installSubscriptionHandlers: true,
       autoSchemaFile: 'schema.gql',
