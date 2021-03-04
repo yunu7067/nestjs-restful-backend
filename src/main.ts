5dd76a24ea2f20fa5dc8740762cf39e24e73747c
yunu7067 <yunu7067@gmail.com>
2021-03-04 22:05:03 +0900

    .

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
-
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
